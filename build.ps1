c11543a5b3ad5ddb079a88e3a735f9d970fcd2c9
Trevor Sullivan <trevor@trevorsullivan.net>
2022-03-22 05:18:47 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# NOTE: Run this script to build the project and push it to a remote registry.
+#       You must already be authenticated to the remote registry.
+
 $Registry = 'registry.digitalocean.com/cbtnuggets'
 $Tag = 'pode-server'
 $RemoteTag = '{0}/{1}' -f $Registry, $Tag
